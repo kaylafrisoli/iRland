903dbc0ff74241edfbfa4e9169aa5b0edefa9fd4
Kayla Frisoli <kfrisoli@andrew.cmu.edu>
2018-02-27 22:44:35 -0500

    Add initial iRland package

---
+++ R/LoadClean.R
@@ -0,0 +1,79 @@
+#' Consistently load raw Irish census data
+#'
+#' Consistently load and standardize raw Irish census data
+#'
+#' @param path path to data
+#'
+#' @param file_delim single character used to separate fields within a record; default is " " for .txt files
+#'
+#' @param assignID string of variable names for creating a unique identifier
+#'
+#' @param preProcess has the data been preprocessed?
+#'
+#' @return data in tibble format
+#'
+#' @examples
+#' LoadCleanRaw("~/GoogleDrive/irelandData/ticknock_kayla.csv", ",", preProcess = TRUE)
+#' LoadCleanRaw("~/GoogleDrive/irelandData/census_ireland_1901/Carlow/Ticknock.txt")
+#' LoadCleanRaw("~/GoogleDrive/irelandData/census_ireland_1901/Carlow/Ticknock.txt", assignID = c("County", "DED", "Year"))
+LoadCleanRaw <- function(path, file_delim = " ", assignID=NULL, preProcess = FALSE){
+
+  dat <- suppressMessages(read_delim(path, delim = file_delim, trim_ws = TRUE, escape_double = FALSE))
+
+  if(!preProcess){
+    # ticknock_kayla has been pre-processed, but the .txt raw files haven't
+    dat$locations = gsub(" ", "", paste0(dat$TownStreet, dat$DED, dat$County))
+    dat$locations = gsub("\\\\", "", dat$locations)
+    dat$locations = gsub("&", "", gsub(",", "", dat$locations))
+    dat$locations = gsub("\'", "", dat$locations)
+
+    dat$household = gsub(" ", "", paste0(dat$TownStreet, dat$DED, dat$County, dat$Number))
+    dat$household = gsub("\\\\", "", dat$household)
+    dat$household = gsub("&", "", gsub(",", "", dat$household))
+    dat$household = gsub("\'", "", dat$household)
+
+    dat$housename = gsub(" ", "", paste0(dat$TownStreet, dat$DED, dat$County, dat$Surname))
+    dat$housename = gsub("\\\\", "", dat$housename)
+    dat$housename = gsub("&", "", gsub(",", "", dat$housename))
+    dat$housename = gsub("\'", "", dat$housename)
+
+    # dat <- dat[!(dat$Forename %in% c("?", "-")), ]
+    dat <- dat[order(dat$Surname, dat$household), ]
+  }
+
+  dat[dat == "-"] <- NA
+  dat$Age <- as.numeric(dat$Age)
+  dat$household_year <- paste0(dat$household,"_", dat$Year)
+
+
+  dat$Literacy[dat$Literacy %in% c("Read write", "Can read and write", "Read and rite",
+                                   "Can read write", "R w", "R and write", "Read with" )] <- "Read and write"
+
+  dat$Literacy[dat$Literacy %in% c("Cannot r and w", "Not read write")] <- "Cannot read or write"
+
+  dat$Literacy[dat$Literacy %in% c("Can read only" , "Read only")] <- "Read"
+
+  dat$MaritalStat[dat$MaritalStat == "Not Married"] <- "Single"
+
+  dat$MaritalStat[is.na(dat$MaritalStat)] <- ""
+
+  dat$County_DED <- paste0(dat$County, ".", dat$DED %>%
+                             gsub("  ", " ", .) %>% gsub(" ", "_", .)) %>%
+    gsub(" ", "", .) %>% gsub("_$", "", .)
+
+  if(!is.null(assignID)){
+    dat$ourID <- apply(dat[, assignID], 1, paste0, collapse="") %>% paste0(".") %>% paste0(1:nrow(dat))
+  }
+
+  return(dat)
+}
+
+
+
+AssignID <- function(dat, variables.for.id){
+
+  dat$ourID <- apply(dat[, variables.for.id], 1, paste0, collapse="") %>% paste0(".") %>% paste0(1:nrow(dat))
+
+  return(dat)
+}
+
