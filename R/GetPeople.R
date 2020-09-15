fbb47d445679fa4eb66d4ba1832f058384946f16
Kayla Frisoli <kfrisoli@andrew.cmu.edu>
2020-09-15 00:25:26 -0400

    new updates

--- R/GetPeople.R
+++ R/GetPeople.R
@@ -138,7 +138,9 @@ ExtractDataByLocation <- function(County_DEDs,
     which_files <- which(stringr::str_replace_all(all_county_deds, "[^[:alnum:]]", "") %in%
                            stringr::str_replace_all(County_DEDs, "[^[:alnum:]]", ""))
 
-    tbl1 = lapply(full_file_paths[which_files], LoadCleanRaw, assignID = c("County", "DED", "Year"))
+    tbl1 = lapply(full_file_paths[which_files], LoadCleanRaw,
+                  assignID = c("County", "DED", "Year"))
+
     # for(i in 1:length(tbl1)){
     #   tbl1[[i]] <- mutate_all(tbl1[[i]], as.character)
     # }
@@ -154,6 +156,7 @@ ExtractDataByLocation <- function(County_DEDs,
 
   return(data_within_radius)
 }
+
 
 
 # other options: "~/GoogleDrive/irelandData/census_ireland_1901"
@@ -283,3 +286,81 @@ GetHousehold <- function(data_candidate, data_household=NULL){
 
 
 
+ExtractDataByLocation_fread <- function(County_DEDs,
+                                  path_1901 = "data/census_ireland_1901",
+                                  path_1911 = "data/census_ireland_1911",
+                                  Pre_Loaded_Data = NULL){
+
+  if(is.null(Pre_Loaded_Data)){
+
+    full_file_paths <- c(list.files(path_1901,
+                                    pattern = "*.txt",
+                                    recursive = TRUE,
+                                    full.names = TRUE,
+                                    include.dirs = TRUE),
+                         list.files(path_1911,
+                                    pattern = "*.txt",
+                                    recursive = TRUE,
+                                    full.names = TRUE,
+                                    include.dirs = TRUE))
+
+    # Extract the DEDs -- district electoral division
+
+    DEDs_to_match_data <- c(list.files(path_1901,
+                                       pattern = "*.txt",
+                                       recursive = TRUE,
+                                       full.names = FALSE,
+                                       include.dirs = FALSE),
+                            list.files(path_1911,
+                                       pattern = "*.txt",
+                                       recursive = TRUE,
+                                       full.names = FALSE,
+                                       include.dirs = FALSE)) %>%
+      stringr::str_extract( "([^/]+$)") %>%
+      gsub(pattern=".txt", replacement="") %>%
+      gsub(pattern="_", replacement=" ")
+
+    counties <- c(list.files(path_1901,
+                             pattern = "*.txt",
+                             recursive = TRUE,
+                             full.names = TRUE,
+                             include.dirs = FALSE),
+                  list.files(path_1911,
+                             pattern = "*.txt",
+                             recursive = TRUE,
+                             full.names = TRUE,
+                             include.dirs = FALSE)) %>%
+      gsub(pattern="([^/]+$)", replacement="") %>%
+      gsub(pattern="/$", replacement="") %>%
+      stringr::str_extract( "([^/]+$)")
+
+    all_county_deds <- paste0(counties, ".", DEDs_to_match_data %>%
+                                gsub(pattern="  ", replacement=" ") %>%
+                                gsub(pattern=" ", replacement="_")) %>%
+      gsub(pattern=" ", replacement="") %>%
+      gsub(pattern="_$", replacement="")
+
+    # 10/8/18 fix some of the grepl issues with county names etc.
+
+    if("Kildare.Athy_East" %in% County_DEDs){
+      County_DEDs <- c(County_DEDs, "Kildare.Athy_East_Urban")
+    }
+    # which_files <- which(all_county_deds %in% County_DEDs)
+    which_files <- which(stringr::str_replace_all(all_county_deds, "[^[:alnum:]]", "") %in%
+                           stringr::str_replace_all(County_DEDs, "[^[:alnum:]]", ""))
+
+    tbl1 = lapply(full_file_paths[which_files], LoadCleanRaw_fread, assignID = c("County", "DED", "Year"))
+    # for(i in 1:length(tbl1)){
+    #   tbl1[[i]] <- mutate_all(tbl1[[i]], as.character)
+    # }
+    data_within_radius = tbl1 %>% dplyr::bind_rows()
+    # rbind.fill was slightly faster so we'll stick to that
+    #data_within_radius = tbl1 %>% rbind.fill()
+
+
+  }else{
+    data_within_radius = dplyr::filter(Pre_Loaded_Data,
+                                       .data$County_DED %in% County_DEDs)
+  }
+  return(data_within_radius)
+}
--- R/LoadClean.R
+++ R/LoadClean.R
@@ -87,8 +87,8 @@ LoadCleanRaw <- function(path, file_delim = " ", assignID=NULL, preProcess = FAL
                 "Birthplace", "Occupation",
                 "Literacy",   "IrishLang",  "MaritalStat", "SpecIlliness",
                 "YearsMarr",  "ChildBorn",  "ChildLiv",
-                "locations",  "household",  "housename",  "household_year", "
-                County_DED", "ourID")
+                "locations",  "household",  "housename",  "household_year",
+                "County_DED", "ourID")
 
   missing_names <- which(!(names_all %in% names(dat)))
 
@@ -113,3 +113,130 @@ AssignID <- function(dat, variables.for.id){
   return(dat)
 }
 
+
+
+
+
+
+#' Consistently load raw Irish census data
+#'
+#' Consistently load and standardize raw Irish census data
+#'
+#' @param path path to data
+#'
+#' @param file_delim single character used to separate fields within a record; default is " " for .txt files
+#'
+#' @param assignID string of variable names for creating a unique identifier
+#'
+#' @param preProcess has the data been preprocessed?
+#'
+#' @return data in tibble format
+#'
+#' @examples
+#' LoadCleanRaw("~/GoogleDrive/irelandData/ticknock_kayla.csv", ",", preProcess = TRUE)
+#' LoadCleanRaw("~/GoogleDrive/irelandData/census_ireland_1901/Carlow/Ticknock.txt")
+#' LoadCleanRaw("~/GoogleDrive/irelandData/census_ireland_1901/Carlow/Ticknock.txt",
+#'  assignID = c("County", "DED", "Year"))
+#' @export
+LoadCleanRaw_fread <- function(path, file_delim = " ", assignID=NULL, preProcess = FALSE){
+    dat <- suppressWarnings(suppressMessages(data.table::fread(path,
+                             strip.white = TRUE,
+                             colClasses = "character")))
+
+  if(!preProcess){
+    # ticknock_kayla has been pre-processed, but the .txt raw files haven't
+    dat$locations = gsub(" ", "", paste0(dat$TownStreet, dat$DED, dat$County))
+    dat$locations = gsub("\\\\", "", dat$locations)
+    dat$locations = gsub("&", "", gsub(",", "", dat$locations))
+    dat$locations = gsub("\'", "", dat$locations)
+
+    dat$household = gsub(" ", "", paste0(dat$TownStreet, dat$DED, dat$County, dat$Number))
+    dat$household = gsub("\\\\", "", dat$household)
+    dat$household = gsub("&", "", gsub(",", "", dat$household))
+    dat$household = gsub("\'", "", dat$household)
+
+    dat$housename = gsub(" ", "", paste0(dat$TownStreet, dat$DED, dat$County, dat$Surname))
+    dat$housename = gsub("\\\\", "", dat$housename)
+    dat$housename = gsub("&", "", gsub(",", "", dat$housename))
+    dat$housename = gsub("\'", "", dat$housename)
+
+    # dat <- dat[!(dat$Forename %in% c("?", "-")), ]
+    dat <- dat[order(dat$Surname, dat$household), ]
+  }
+
+  dat[dat == "-"] <- NA
+  dat$Age <- as.numeric(dat$Age)
+  dat$Year <- as.numeric(dat$Year)
+
+  dat$household_year <- paste0(dat$household,"_", dat$Year)
+
+
+  dat$Literacy[dat$Literacy %in% c("Read write", "Can read and write",
+                                   "Read and rite", "Can read write",
+                                   "R w", "R and write",
+                                   "Read with" )] <- "Read and write"
+
+  dat$Literacy[dat$Literacy %in% c("Cannot r and w",
+                                   "Not read write")] <- "Cannot read or write"
+
+  dat$Literacy[dat$Literacy %in% c("Can read only" , "Read only")] <- "Read"
+
+  dat$MaritalStat[dat$MaritalStat == "Not Married"] <- "Single"
+
+  dat$MaritalStat[is.na(dat$MaritalStat)] <- ""
+
+  dat$County_DED <- paste0(dat$County, ".",
+                           dat$DED %>%
+                             gsub(pattern="  ", replacement=" ") %>%
+                             gsub(pattern=" ", replacement="_")) %>%
+    gsub(pattern=" ", replacement="") %>%
+    gsub(pattern="_$", replacement="")
+
+  if(!is.null(assignID)){
+    dat$ourID <- apply(dat[, ..assignID], 1, paste0, collapse="") %>%
+      paste0(".") %>%
+      paste0(1:nrow(dat))
+  }
+
+  names_all <- c("Year", "County",     "DED",  "TownStreet", "Number",     "ID",
+                 "Surname",
+                 "Forename",   "Age",   "Sex",   "RelationHead",   "Religion",
+                 "Birthplace", "Occupation",
+                 "Literacy",   "IrishLang",  "MaritalStat", "SpecIlliness",
+                 "YearsMarr",  "ChildBorn",  "ChildLiv",
+                 "locations",  "household",  "housename",  "household_year",
+                 "County_DED", "ourID")
+
+  missing_names <- which(!(names_all %in% names(dat)))
+
+  if(length(missing_names) == 0){
+    return(dat)
+  } else{
+    dat[, names_all[missing_names] := NA]
+    return(dat)
+  }
+
+  }
+#
+# }
+
+
+
+
+# path = "data/census_ireland_1901/Dublin/Arran_Quay.txt"
+# file_delim = " "
+# assignID = c("County", "DED", "Year")
+# preProcess = FALSE
+#
+# system.time({
+#   LoadCleanRaw(path = "data/census_ireland_1901/Dublin/Arran_Quay.txt",
+#                file_delim = " ",
+#                assignID = c("County", "DED", "Year"),
+#                preProcess = FALSE)
+# })
+#
+# system.time({
+#   LoadCleanRaw_fread(path = "data/census_ireland_1901/Dublin/Arran_Quay.txt",
+#                      assignID = c("County", "DED", "Year"),
+#                      preProcess = FALSE)
+# })
